79950776ad5620b05fa8b95a4538dd161c3e52ab
thaleswillreis <thaleswill@yahoo.com.br>
2024-07-26 18:39:21 -0300

    feat: Data scraping completed

--- src/raspagem_ml/spiders/mercadolivre.py
+++ src/raspagem_ml/spiders/mercadolivre.py
@@ -4,12 +4,14 @@ import scrapy
 class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     start_urls = ["https://lista.mercadolivre.com.br/intelbras"]
+    pag_inicial = 1
+    pag_final = 10
 
     def parse(self, response):
         itens = response.css('div.ui-search-result__content')
 
         for item in itens:
-            
+
             precos = item.css('span.andes-money-amount__fraction::text').getall()
             centavos = item.css('span.andes-money-amount__cents::text').getall()
 
@@ -20,3 +22,10 @@ class MercadolivreSpider(scrapy.Spider):
                 'preco': precos[1] if len(precos) > 1 else None,
                 'preco_centavos': centavos[1] if len(centavos) > 1 else None
             }
+
+        if self.pag_inicial < self.pag_final:
+            if proxima_pagina := response.css(
+                'li.andes-pagination__button.andes-pagination__button--next a::attr(href)'
+            ).get():
+                self.pag_inicial += 1
+                yield scrapy.Request(url=proxima_pagina, callback=self.parse)
